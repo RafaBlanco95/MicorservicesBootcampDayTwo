76a0a7d4f17f7de19a752efbefd5a9e120c445d7
RafaelBlanco95 <rafa.blanco.95@gmail.com>
2023-02-07 08:41:12 +0100

    SingletonPatternAdded

---
+++ design-patterns/src/main/java/com/nttdata/design/pattern/DesignPatternMain.java
@@ -0,0 +1,17 @@
+package com.nttdata.design.pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class DesignPatternMain 
+{
+    public static void main( String[] args )
+    {
+        
+    	
+    	Person Rafa=Person.getInstance();
+        Rafa.setName("Rafa");
+    	
+    }
+}
